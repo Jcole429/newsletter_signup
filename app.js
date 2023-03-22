27f17dd20987f9587b7c6f76561effc8d0f31d99
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-22 12:14:23 -0400

    Add subscribingUser to mailchimp audience when form is submitted

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-const app = express()
+const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mailchimp.setConfig({
+    apiKey: mailchimpApiKey,
+    server: mailchimpServerPrefix,
+});
 
 app.listen(port, () => {
     console.log("App listening on port " + port);
@@ -19,5 +26,21 @@ app.post("/", (req, res) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
-    console.log(req.body);
+
+    const subscribingUser = {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+        }
+    }
+
+    mailchimpAddContact(subscribingUser);
 });
+
+async function mailchimpAddContact(subscribingUser) {
+    const response = await mailchimp.lists.addListMember(mailchimpAudienceId, subscribingUser);
+    console.log(response);
+    console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
+}
